6d7951352f049ff4e6af27f20371a343b81044c7
Spegelius <spegelius@gmail.com>
2015-01-08 22:53:46 +0200

    wpa_suplicant updates
    Change-Id: I7149f18832fbb3a00c00b6fc41db09a34895240f

--- jf-common.mk
+++ jf-common.mk
@@ -232,14 +232,11 @@ PRODUCT_PACKAGES += \
     macloader \
     dhcpcd.conf \
     hostapd \
-    hostapd_default.conf \
-    libwpa_client \
-    wpa_supplicant
+    wpa_supplicant \
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 # audio
